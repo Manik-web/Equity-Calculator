a9d230699331d338eefcf01a19fa26cc12f07381
Manik <sundeepmanik83@gmail.com>
2020-08-21 16:52:58 +0100

    added a simple calc

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const initValue;
+const initValue = 150000;
+const currentValue = 149999;
+//const mortgageEquityGain;
+
+
+let propertyGain = (initValue, currentValue) =>{
+    let gain = currentValue - initValue;
+
+    if (gain>0){
+        console.log(`You have gained £${gain} in equity alone!`);
+    }else if (gain === 0){
+        console.log('You have neither gained or lost money yet!')
+    } else {
+       // gain = abs(gain);
+        console.log(`Unfortunately, you have lost £${Math.abs(gain)} in equity`);
+    }
+}
+
+propertyGain(initValue, currentValue);
