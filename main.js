eb8eafb1a6887977789a8f5f82cdbd4e0829300e
Manik <sundeepmanik83@gmail.com>
2020-08-22 21:11:52 +0100

    basic form added and centered

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const initMortgage = 142500;
 const remMortgage = 136323;
 const initDeposit = initValue-initMortgage;
 
-
+//calculate the gain in property from prop prices
 let propertyGain = (initValue, currentValue) =>{
     let gain = currentValue - initValue;
 
@@ -18,16 +18,14 @@ let propertyGain = (initValue, currentValue) =>{
     }
     return gain;
 }
-
-
-
+//calc the total capital repayment
 let mortgageGain = (initMortgage, remMortgage) =>{
     let morGain = initMortgage - remMortgage;
     console.log(`You have paid off £${morGain} of your mortgage so far`);
     return morGain;
 }
 
-
+//calculate the total gain including the initial mortgage
 let totalGain = (equity, mortgage) =>{
     deposit = equity+mortgage+initDeposit
     console.log(`In total you have £${deposit} in equity in your property`);
