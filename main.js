99fbe5cb1c9a80818ae61037db5b69948d8b1a64
Manik-web <sundeepmanik83@gmail.com>
2020-09-12 21:06:21 +0100

    fixed and regular maths working

--- main.js
+++ main.js
@@ -73,31 +73,49 @@ function displayResult (deposit){
 ;}
 
 //testing the monthly payments section
-let mortgageAmount = 244450;
-let interestRate = 1.8;
-let intCalc = interestRate/1200;
-let loanTerm = 33;
+let mortgageAmount = 100000;
+let loanTerm = 25;
 
-let intRate = 1+intCalc;
+/*fixed rate section*/
+let fixedLength = 2;
+let fixedIntRate = 1.34;
+let fixedCalc = 1+(fixedIntRate/1200);
+let fixCalc1 = Math.pow(fixedCalc,(loanTerm*12));
+let fixCalc2 = Math.pow(fixedCalc,(loanTerm*12))-1;
+let fixedMonthly = ((mortgageAmount*fixedIntRate)/1200)*(fixCalc1/fixCalc2);
+console.log('monthly rate for offer '+fixedMonthly);
+
+loanTerm-=fixedLength;
+/*rest of term*/
+
+let interestRate = 4.35;
+let intRate = 1+(interestRate/1200);
 let calc1 = Math.pow(intRate,(loanTerm*12));
 let calc2 = Math.pow(intRate,(loanTerm*12))-1;
-console.log(calc1,calc2);
 
-let monthlyRepayment = ((mortgageAmount*interestRate)/1200)*(calc1/calc2);
-console.log(monthlyRepayment);
-
-let debt = mortgageAmount;
 let monthEquity = 0;
-let monthInterest;
 
-for (i=1;i<(loanTerm*12)+1;i++){
-    debt *=intRate;
-    // debt += monthInterest;
-   // console.log('debt after interest ' + debt);
-    debt-=monthlyRepayment;
-   // console.log('debt after payment ' + debt);
-   
-    monthEquity = mortgageAmount-debt;
-    //console.log(debt);
-    console.log(`after ${i} months you have ${Math.floor(monthEquity*100)/100} in equity`);
+let fixedCalculation=(rate, length, loan)=>{
+    for(j=1;j<(length*12)+1;j++){
+        loan *= rate;
+        loan -= fixedMonthly;
+        monthEquity =mortgageAmount-loan;
+        console.log(`after ${j} months you have ${Math.floor(monthEquity*100)/100} in equity`);
+    }
+    return loan;
 }
+let debt = fixedCalculation(fixedCalc,fixedLength,mortgageAmount);
+console.log(loanTerm);
+
+
+mortgageEquityCalculation = (intRate, loanTerm,debt)=>{
+    for (i=1;i<(loanTerm*12)+1;i++){
+        debt *=intRate;
+        debt-=monthlyRepayment;
+        monthEquity = mortgageAmount-debt;
+        console.log(`after ${(fixedLength*12)+i} months you have ${Math.floor(monthEquity*100)/100} in equity`);
+}
+}
+let monthlyRepayment = ((debt*interestRate)/1200)*(calc1/calc2);
+console.log('monthly rate for regular mortgage ' +monthlyRepayment);
+mortgageEquityCalculation(intRate,loanTerm, debt);
